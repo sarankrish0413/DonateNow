f16e4e4335cb9b92d3327e022be13012b111084b
gayathrp <gayathrii.gayathrii@gmail.com>
2017-02-21 18:27:52 -0800

    With reviews view controller and swift files

---
+++ DonateNow/RequestorWriteReviewsController.swift
@@ -0,0 +1,16 @@
+//
+//  RequestorWriteReviewsController.swift
+//  DonateNow
+//
+//  Created by Gayathri Palanisami on 2/21/17.
+//  Copyright © 2017 Saranya Krishnan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RequestorWriteReviewsController : UIViewController {
+    @IBOutlet weak var reviewTextView: UITextView!
+    @IBAction func submitButtonAction(_ sender: UIButton) {
+    }
+}
